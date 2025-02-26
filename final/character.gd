dc47144a25b11dd7c6962ff6f016ff15988af561
Ajah Leggett <al191927@ebrschools.org>
2025-02-26 08:25:05 -0600

    got animation player to start moving through each image, added state machine player and functions

--- final/character.gd
+++ final/character.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var smp: Node = $StateMachinePlayer
 
 const SPEED = 120.0
 
@@ -26,6 +27,15 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0: 
 		$AnimatedSprite2D.flip_h = true
 	
+	smp.set_param('moving', (abs(direction) + abs(direction2)) != 0)
+	match curr_state: 
+		"standing":
+			standing()
+		"walking":
+			walking()
+		'attack':
+			attack()
+		
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -38,7 +48,33 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
+	
 	move_and_slide()
+
+func walking():
+	$AnimatedSprite2D.play('walking right')
+	pass
+	
+func attack():
+	print("bang")
+	if $AnimatedSprite2D.animation != 'attack right':
+		$AnimatedSprite2D.play('attack right')
+	pass
+	
+func death():
+	pass
+	
+func standing():
+	$AnimatedSprite2D.play('standing')
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	
+	
+	if event is InputEventKey and event.pressed and event.keycode == 32:
+		smp.set_trigger('attack')
+		
 
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
@@ -48,4 +84,15 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	print('player body entered')
+	pass # Replace with function body.
+
+var curr_state
+func _on_state_machine_player_updated(state: Variant, delta: Variant) -> void:
+	curr_state = state 
+	pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == 'attack right':
+		smp.set_trigger('attack end')
 	pass # Replace with function body.
